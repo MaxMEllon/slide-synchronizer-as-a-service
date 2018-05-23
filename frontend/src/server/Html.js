ea58f4b9ea9e0d07a9abbd84f1afed546d60bc02
MaxMEllon <maxmellon1994@gmail.com>
2018-05-23 21:23:41 +0900

    Format html

--- frontend/src/server/Html.js
+++ frontend/src/server/Html.js
@@ -8,29 +8,29 @@ const styleTags = new ServerStyleSheet().getStyleTags()
 
 export default function renderFullPage() {
   return `
-    <!DOCTYPE html>
-    <html>
-      <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
-        <meta charset="UTF-8">
-        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=0.5,maximum-scale=3,minimal-ui"
-        />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="theme-color" content="#317EFB" />
-        <meta property="og:title" content="<%= htmlWebpackPlugin.options.title %>" />
-        <meta property="og:type" content="article" />
-        <meta property="og:url" content="https://example.com" />
-        ${styleTags}
-      </head>
-      <body>
-        <div id="root">${renderedContent}</div>
-        <script type="text/javascript" src="${process.env.CLIENT_JS_LOC}/client.js"></script>
-        <script type="text/javascript" src="${process.env.CLIENT_JS_LOC}/vendor.client.js"></script>
-      </body>
-    </html>
+<!DOCTYPE html>
+<html>
+  <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width,initial-scale=1,minimum-scale=0.5,maximum-scale=3,minimal-ui"
+    />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="theme-color" content="#317EFB" />
+    <meta property="og:title" content="<%= htmlWebpackPlugin.options.title %>" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://example.com" />
+    ${styleTags}
+  </head>
+  <body>
+    <div id="root">${renderedContent}</div>
+    <script type="text/javascript" src="${process.env.CLIENT_JS_LOC}/client.js"></script>
+    <script type="text/javascript" src="${process.env.CLIENT_JS_LOC}/vendor.client.js"></script>
+  </body>
+</html>
   `
 }
