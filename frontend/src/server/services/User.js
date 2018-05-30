b6f551c521091e51cf76f5e834eee7878afc4fc2
MaxMEllon <maxmellon1994@gmail.com>
2018-05-30 21:57:27 +0900

    Implemented fetchr of user

--- frontend/src/server/services/User.js
+++ frontend/src/server/services/User.js
@@ -13,7 +13,13 @@ export default class User extends Service {
   create(req: any, resource: any, params: any, body: any) {
     return this.axios.post('/users/sign_up', snakeCaseKeys(body))
   }
-  update(req: any, resource: any, params: any, body: any) {}
 
-  delete(req: any, resource: any, params: any, body: any) {}
+  update(req: any, resource: any, params: any, body: any) {
+    return this.axios.post('/users/sign_in', snakeCaseKeys(body))
+  }
+
+  delete(req: any, resource: any, params: any, body: any) {
+    const path = `/usets/sign_out?token=${params.token}`
+    return this.axios.post(path, snakeCaseKeys(body))
+  }
 }
